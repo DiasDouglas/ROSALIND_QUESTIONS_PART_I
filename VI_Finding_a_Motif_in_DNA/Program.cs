a8fbce83bcbc3cd4d41de71cba84601c49031507
Douglas Dias <douglas.dias2102@outlook.com>
2021-05-18 02:49:52 -0300

    Adding solutions for question 6, Finding a Motif in DNA

---
+++ VI_Finding_a_Motif_in_DNA/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace VI_Finding_a_Motif_in_DNA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dataset = File.ReadAllText("rosalind_subs.txt");
+
+            var parameters = dataset.Replace("\r", "").Split('\n');
+
+            var dnaString = parameters[0];
+            var dnaSubstring = parameters[1];
+            var substringSize = dnaSubstring.Length;
+            string positions = "";
+
+            for(int i = 0; i + substringSize < dnaString.Length; i++)
+            {
+                if(dnaString.Substring(i, substringSize) == dnaSubstring)
+                {
+                    if (positions.Length != 0)
+                        positions += $" {i + 1}";
+                    else
+                        positions = $"{i + 1}";
+                }
+            }
+
+            Console.WriteLine(positions);
+        }
+    }
+}
